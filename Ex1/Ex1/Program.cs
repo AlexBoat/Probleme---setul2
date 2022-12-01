40120f8801c625dcc1f1d1bdd69ffb79e9cab4d6
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-12-01 22:00:22 +0200

    Add files via upload

---
+++ Ex1/Ex1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, nr=0;
+            n = Convert.ToInt32(Console.ReadLine());
+            for(int i=1; i<=n; i++)
+            {
+                x = Convert.ToInt32(Console.ReadLine());
+                if(x%2==0)
+                {
+                    nr++;
+                }
+            }
+            Console.WriteLine(nr);
+            Console.Read();
+        }
+    }
+}
