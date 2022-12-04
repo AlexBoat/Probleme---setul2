d2c50c38a639c9ddfc7944622b06c25a47ed7496
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-12-04 19:35:35 +0200

    Add files via upload

---
+++ Ex6/Ex6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n , x1, x2;
+            bool ok = true;
+            n = Convert.ToInt32(Console.ReadLine());
+            x1 = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                x2 = Convert.ToInt32(Console.ReadLine());
+                if (x1>x2)
+                {
+                    ok = false;
+                }
+                x1 = x1 + x2;
+                x2 = x1 - x2;
+                x1 = x1 - x2;
+            }
+            if (ok == true)
+                Console.WriteLine("DA");
+            else
+                Console.WriteLine("NU");
+            Console.Read();
+        }
+    }
+}
---
+++ Ex7/Ex7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, min=int.MaxValue, max=int.MinValue;
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                x = Convert.ToInt32(Console.ReadLine());
+                if(x > max)
+                {
+                    max = x;
+                }
+                if(x < min)
+                {
+                    min = x;
+                }
+            }
+            Console.WriteLine(min + " " + max);
+            Console.Read();
+        }
+    }
+}
