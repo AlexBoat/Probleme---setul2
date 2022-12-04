439ed2c881b3fa644e9727db35fb69089de5e2b4
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-12-04 19:33:52 +0200

    Add files via upload

---
+++ Ex4/Ex4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a, x, poz=-1;
+            n = Convert.ToInt32(Console.ReadLine());
+            a = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i<n; i++)
+            {
+                x = Convert.ToInt32(Console.ReadLine());
+                if (x == a)
+                {
+                    poz = i;
+                }
+            }
+            Console.WriteLine("{0} se afla pe pozitia {1}", a, poz);
+            Console.Read();
+            
+        }
+    }
+}
---
+++ Ex5/Ex5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, nr = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                x = Convert.ToInt32(Console.ReadLine());
+                if (x == i)
+                {
+                    nr++;
+                }
+            }
+            Console.WriteLine("{0} elemente", nr);
+            Console.Read();
+        }
+    }
+}
