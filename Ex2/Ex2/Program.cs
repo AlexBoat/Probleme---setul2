4a47abe0282fe933df973e1ae8256a8adc468984
AlexBoat <117096647+AlexBoat@users.noreply.github.com>
2022-12-04 19:32:02 +0200

    Add files via upload

---
+++ Ex2/Ex2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, nrneg = 0, nrzero=0, nrpoz=0;
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                x = Convert.ToInt32(Console.ReadLine());
+                if (x<0)
+                {
+                    nrneg++;
+                }
+                else
+                {
+                    if(x==0)
+                    {
+                        nrzero++;
+                    }
+                    else
+                    {
+                        nrpoz++;
+                    }
+
+                }
+            }
+            Console.WriteLine(nrneg + " " + nrzero + " " + nrpoz);
+            Console.Read();
+        }
+    }
+}
---
+++ Ex3/Ex3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, p=1;
+            n= Convert.ToInt32(Console.ReadLine());
+            Console.Write((n*(n+1))/2 + " ");
+            for(int i = 2; i <= n; i++)
+            {
+                p *= i;
+            }
+            Console.Write(p);
+            Console.Read();
+        }
+    }
+}
